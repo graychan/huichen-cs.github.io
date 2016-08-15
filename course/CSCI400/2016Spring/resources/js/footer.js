59a09ebcbe097657708339a1b83a35a7ce5513a7
Hui Chen <gray.chenhui@gmail.com>
2016-08-15 09:23:09 -0400

    initialization for fall 2016

---
+++ course/CSCI400/2016Spring/resources/js/footer.js
@@ -0,0 +1,9 @@
+document.write('<p class="copyright"><small>Hui Chen &copy;' 
+        + ((new Date(document.lastModified)).getYear() + 1900) 
+        + '</small></p>');
+document.write('<p><small>');
+// document.write(document.title + '<br/>');
+document.write('Last modified: ' + 
+document.lastModified + '<br/>');
+document.write('</small></p>');
+
